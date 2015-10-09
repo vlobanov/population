80f8c62c165cd31a60951f2fa9e5236a95d744d7
Vadim Lobanov <vadim@lobanov.pw>
2015-10-09 17:21:13 +0200

    compute density for given latitude/longtitude and radius

--- src/population/density.clj
+++ src/population/density.clj
@@ -88,38 +88,90 @@
     (long (/ (- longtitude x-corner)
              cell-size))))
 
+(defn latitude-sin [latitude]
+  (-> latitude
+      (+ 90.0)
+      Math/toRadians
+      Math/sin))
+
+(defn cell-size-no-distortion-km []
+  (* (/ (:cell-size @density-description)
+        180.0)
+      Math/PI
+      earth-r-km))
+
+(defn- density-in-cell [r c]
+  (let [{:keys [rows cols]} @density-description]
+    (get-in @density-data [(mod r rows)
+                           (mod c cols)])))
+
+(defn offsets-distance
+  "Measures distance in some arbitrary units between cells.
+   width of cell is smaller than height by sin(latitude),
+   so we account for thar"
+  [lat-sin r c]
+  (let [c-k (* lat-sin c)]
+    (+ (* r r)
+       (* c-k c-k))))
+
+(defn offsets-for
+  "Figures out how many cells up/down and right/left we have to look
+   to cover circle on given radius"
+  [lat-sin radius-km]
+  (let [row-offset (Math/ceil
+                     (/ radius-km
+                        (cell-size-no-distortion-km)))
+        column-offset (Math/ceil
+                        (/ row-offset lat-sin))]
+    [(long row-offset) (long column-offset)]))
+
+(defn densities-around
+  "get a list of pairs (population density, distance to given point)"
+  [latitude longtitude radius-km]
+  (let [lat-sin (latitude-sin latitude)
+        [r-cells-offset c-cells-offset] (offsets-for lat-sin radius-km)
+        c (longtitude->column-index longtitude)
+        r (latitude->row-index latitude)]
+    (for [r-offset (range (- r-cells-offset) (+ r-cells-offset 1))
+          c-offset (range (- c-cells-offset) (+ c-cells-offset 1))]
+      (let [dist (offsets-distance lat-sin r-offset c-offset)
+            density (density-in-cell (+ r-offset r)
+                                     (+ c-offset c))]
+        [dist density]))))
+
+(defn circle-surface-area [radius-km]
+  (* Math/PI (* radius-km radius-km)))
+
 (defn cell-surface-area
   "we know size of grid cell in degrees,
    but may need to get its surface area"
   [latitude]
-  (let [k (-> latitude
-              (+ 90.0)
-              Math/toRadians
-              Math/sin)
-        c-s (* (/ (:cell-size @density-description)
-                  180.0)
-                Math/PI
-                earth-r-km)]
+  (let [k (latitude-sin latitude)
+        c-s (cell-size-no-distortion-km)]
     (* c-s c-s k)))
 
-; (defn coverage-s [km lat]
-;   (let [a (Math/sqrt km)
-;         n (/ a
-;              (cell-surface-area lat))]
-;     (log/info "n=" n)
-;     (int (/ n 2))))
+(defn population-in-cell [area cell-area cell-density]
+  (* (if (> area cell-area) cell-area area)
+     cell-density))
 
-; (defn at [lat longt km]
-;   (let [r (latitude->row-index lat)
-;         c (longtitude->column-index longt)
-;         d (coverage-s km lat)
-;         rg (range (- d) (+ d 1))]
-;     (mapv
-;       (fn [x]
-;           (mapv
-;             (fn [y]
-;               (get-in @density-data [(+ x r) (+ y c)]))
-;             rg))
-;       rg)))
-
-; (defn find-density-at [latitude longtitude])
+(defn count-population
+  "Actually does what it says.
+   First we get all cells close to given point,
+   Sort by distance and in greedy manner while we
+   still have some area left multiply density by area"
+  [latitude longtitude radius-km]
+  (let [densities (->> (densities-around latitude longtitude radius-km)
+                       (sort-by first)
+                       (map last))
+        cell-area (cell-surface-area latitude)]
+    (loop [left-area (circle-surface-area radius-km)
+           population 0
+           densities densities]
+      (if (or (<= left-area 0) (empty? densities))
+          (long population)
+          (recur (- left-area cell-area)
+                 (+ population
+                    (population-in-cell left-area
+                                        cell-area
+                                        (first densities)))
+                 (rest densities))))))
