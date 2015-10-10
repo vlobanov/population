9172fa003430acd8d035e41313f369898526319a
Vadim Lobanov <vadim@lobanov.pw>
2015-10-10 13:23:13 +0200

    webserver API handler

--- src/population/core.clj
+++ src/population/core.clj
@@ -1,7 +1,63 @@
 (ns population.core
-  (:gen-class))
+  (:gen-class)
+  (:require [population.density :as density]
+            [population.cities :as cities]
+            [taoensso.timbre :as log]
+            [clojure.core.match :refer [match]]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [immutant.web :refer [run run-dmc]]
+            [clj-json.core :refer [generate-string]]
+            [ring.middleware.params :refer :all]))
+
+(defn too-many-cities-found [cities]
+  (map (fn [{:keys [city country region] :as city-data}]
+         (assoc city-data :full_query (str country "," region "," city)))
+       cities))
+
+(defn get-population-with-radius [city radius]
+  (let [cities (cities/find-city city)]
+    (if (= (count cities) 1)
+      (let [[{:keys [latitude longitude] :as city}] cities
+            population (density/count-population (double latitude)
+                                                 (double longitude)
+                                                 radius)]
+        [:ok {:population population
+              :radius radius
+              :city city}])
+      [:error {:description (str "found " (count cities) " results, expected 1")
+               :results (too-many-cities-found cities)}])))
+
+(defn get-population [city-search radius-str]
+  (if-let [radius (and (string? radius-str)
+                       (re-matches #"^\d+$" radius-str)
+                       (Long. radius-str))]
+    (if (< radius density/max-radius)
+      (if (not-empty city-search)
+          (get-population-with-radius city-search radius)
+          [:error "city is incorrect"])
+      [:error ("radius must be less than " density/max-radius " km")]
+    [:error "radius is incorrect"]))
+
+(defn json [status data]
+  {:status status
+   :headers {"Content-Type" "application/json; charset=UTF-8"}
+   :body (generate-string data)})
+
+(defn handler [{{:strs [city radius]} :params}]
+  (log/info [city radius])
+  (match (get-population city radius)
+         [:error error-explanation] (json 422 {:error error-explanation})
+         [:ok result] (json 200 result)))
+
+(defroutes app
+  (GET "/city/population" request (handler request))
+  (route/resources "/")
+  (route/not-found {:status 404 :body "not found"}))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (density/read-ascii-density-file! "data/glds15ag.asc")
+  (-> app
+      wrap-params
+      run))
